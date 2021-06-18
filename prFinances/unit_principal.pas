bd91af5cd08ddab61fb59702a23706a2138a1c16
Diuari.molinari <Diuari.molinari@gmail.com>
2021-06-18 19:34:29 -0300

    Adicionando evento no comando novo para apresentar form de transacao

--- prFinances/unit_principal.pas
+++ prFinances/unit_principal.pas
@@ -6,7 +6,7 @@ uses
   System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
   FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs,
   FMX.Controls.Presentation, FMX.StdCtrls, System.Rtti, FMX.Grid.Style,
-  FMX.ScrollBox, FMX.Grid;
+  FMX.ScrollBox, FMX.Grid, unit_transacao;
 
 type
   TfrmPrincipal = class(TForm)
@@ -21,6 +21,7 @@ type
     CurrencyColumn1: TCurrencyColumn;
     DateColumn1: TDateColumn;
     btnNovo: TButton;
+    procedure btnNovoClick(Sender: TObject);
   private
     { Private declarations }
   public
@@ -33,5 +34,13 @@ var
 implementation
 
 {$R *.fmx}
+
+procedure TfrmPrincipal.btnNovoClick(Sender: TObject);
+begin
+  if frmTrancacao = nil then
+    frmTrancacao := frmTrancacao.Create(self);
+
+  frmTrancacao.Show;
+end;
 
 end.
