5645febb526734a015d87f5b5f0008480e0c95ba
Diuari.molinari <Diuari.molinari@gmail.com>
2021-06-18 22:47:24 -0300

    Formulario pronto para receber registro em edicao. Formulario esta fazendo crud completo.

--- prFinances/Form/Frm_Principal.pas
+++ prFinances/Form/Frm_Principal.pas
@@ -6,7 +6,7 @@ uses
   System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
   FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs,
   FMX.Controls.Presentation, FMX.StdCtrls, System.Rtti, FMX.Grid.Style,
-  FMX.ScrollBox, FMX.Grid, frm_transacao;
+  FMX.ScrollBox, FMX.Grid, frm_transacao, Unit_Transacao;
 
 type
   TfrmPrincipal = class(TForm)
@@ -21,7 +21,9 @@ type
     clValor: TCurrencyColumn;
     clData: TDateColumn;
     clDescricao: TStringColumn;
+    IntegerColumn1: TIntegerColumn;
     procedure btnNovoClick(Sender: TObject);
+    procedure StringGrid1CellClick(const Column: TColumn; const Row: Integer);
   private
     { Private declarations }
   public
@@ -36,6 +38,8 @@ implementation
 {$R *.fmx}
 
 procedure TfrmPrincipal.btnNovoClick(Sender: TObject);
+var
+currentRegister: TObj_Transacao;
 begin
   if frmTrancacao = nil then
     frmTrancacao := frmTrancacao.Create(self);
@@ -43,4 +47,19 @@ begin
   frmTrancacao.Show;
 end;
 
+procedure TfrmPrincipal.StringGrid1CellClick(const Column: TColumn;
+  const Row: Integer);
+begin
+    {
+                                           Exemplo de como abrir formulario preenchido com dados do registro
+  currentRegister := TObj_Transacao.Create;
+  currentRegister.setCodigo(PASSAR ID DA LINHA CLICADA);
+  currentRegister.select;
+  frmTrancacao.setCurrentRegister(currentRegister);
+  }
+
+end;
+
 end.
+
+
--- prFinances/Form/Frm_Transacao.pas
+++ prFinances/Form/Frm_Transacao.pas
@@ -18,50 +18,117 @@ type
     Label1: TLabel;
     Label2: TLabel;
     Label3: TLabel;
+    lblCodigo: TLabel;
+    Label4: TLabel;
+    btnLimpar: TButton;
     procedure btnSalvarClick(Sender: TObject);
+    procedure btnLimparClick(Sender: TObject);
+    procedure btnExcluirClick(Sender: TObject);
 
   private
     { Private declarations }
   public
     { Public declarations }
     procedure inserirTransacao;
+    procedure atualizarTransacao;
+    procedure deletarTransacao;
     function validateFields:boolean;
     procedure clearFields();
+    procedure setCurrentRegister(transacao: TObj_Transacao);
+    procedure updateUiByCurrentRegister;
   end;
 
 var
   frmTrancacao: TfrmTrancacao;
+  currentRegister: TObj_Transacao;
+
 
 implementation
 
 {$R *.fmx}
 
+procedure TfrmTrancacao.atualizarTransacao;
+begin
+    currentRegister.setValor(StrToFloat(txtValor.Text));
+    currentRegister.setData(dtData.Date);
+    currentRegister.setDescricao(txtDescricao.Text);
+    currentRegister.update;
+
+    ShowMessage('Registro atualizado com sucesso!');
+end;
+
+procedure TfrmTrancacao.btnExcluirClick(Sender: TObject);
+begin
+  deletarTransacao;
+end;
+
+procedure TfrmTrancacao.btnLimparClick(Sender: TObject);
+begin
+     clearFields;
+     currentRegister := nil;
+end;
+
 procedure TfrmTrancacao.btnSalvarClick(Sender: TObject);
 begin
-  inserirTransacao;
+  if not(validateFields) then
+      exit;
+
+  if currentRegister = nil then
+     inserirTransacao
+  else
+    atualizarTransacao;
+
 end;
 
 procedure TfrmTrancacao.clearFields;
 begin
     txtValor.Text := '';
     txtDescricao.Text := '';
+    lblCodigo.Text := '0';
+end;
+
+procedure TfrmTrancacao.deletarTransacao;
+begin
+   if currentRegister <> nil then
+   begin
+      currentRegister.delete;
+      ShowMessage('Registro excluído com sucesso!');
+      clearFields;
+   end;
+
 end;
 
 procedure  TfrmTrancacao.inserirTransacao;
 var
   transacao : TObj_Transacao;
 begin
-    if not(validateFields) then
-      exit;
-
     transacao := TObj_Transacao.Create;
     transacao.setValor(StrToFloat(txtValor.Text));
     transacao.setData(dtData.Date);
     transacao.setDescricao(txtDescricao.Text);
     transacao.insert;
 
+    transacao.setCodigo(transacao.getUltimoCodigoInserido);
+    lblCodigo.Text := IntToStr(transacao.getCodigo);
     ShowMessage('Registro inserido com sucesso!');
-    clearFields;
+    currentRegister := transacao;
+end;
+
+procedure TfrmTrancacao.setCurrentRegister(transacao: TObj_Transacao);
+begin
+  currentRegister := transacao;
+  updateUiByCurrentRegister;
+end;
+
+procedure TfrmTrancacao.updateUiByCurrentRegister;
+begin
+  if currentRegister <> nil then
+  begin
+    lblCodigo.Text := inttostr(currentRegister.getCodigo);
+    txtValor.Text := FloatToStr(currentRegister.getValor);
+    dtData.Date := currentRegister.getData;
+    txtDescricao.Text := currentRegister.getDescricao;
+  end;
 end;
 
 function TfrmTrancacao.validateFields: boolean;
